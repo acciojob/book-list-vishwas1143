4553cb14ac2fc4e78fd1b70d560b33d229c8358c
vishwas1143 <137210183+vishwas1143@users.noreply.github.com>
2025-03-01 13:24:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+let title=document.getElementById("title").value;
+let author =document.getElementById("author").value;
+let isbn=document.getElementById('isbn').value;
+let addBook=document.getElementById("submit");
+const list = document.getElementById('bookList');
+let table=document.getElementById("myTable");
+	
+addBook.addEventListener('click',()=>{
+	
+    const row = document.creatceElement('tr');
+	const td=document.createElement('td');
+	const td1=document.createElement('td');
+	const td2=document.createElement('td');
+	const td3 =document.createElement('td');
+	
+	const deleteBtn=document.createElement('button');
+	td3.appendChild(deleteBtn)
+	deleteBtn.addEventListerner('click',()=>{
+		list.remove('tr');
+		
+	})
+	td.textContent=title.value;
+	td1.textContent=author.value;
+	td2.textContent=isbn.value;
+	row.appendChild(td);
+	row.appendChild(td1);
+	row.appendChild(td2);
+	row.appendChild(td3);
+	list.appendChild(row);
+	
+	 
+})
