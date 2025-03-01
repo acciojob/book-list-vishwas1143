d4e3cbe1db4b3488e62f937a9bcb2a41be95e48b
vishwas1143 <137210183+vishwas1143@users.noreply.github.com>
2025-03-01 20:00:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-//your JS code here. If required.
-let title=document.getElementById("title").value;
-let author =document.getElementById("author").value;
-let isbn=document.getElementById('isbn').value;
-let addBook=document.getElementById("submit");
-const list = document.getElementById('bookList');
-let table=document.getElementById("myTable");
-	
-addBook.addEventListener('click',()=>{
-	
-    const row = document.creatceElement('tr');
-	const td=document.createElement('td');
-	const td1=document.createElement('td');
-	const td2=document.createElement('td');
-	const td3 =document.createElement('td');
-	
-	const deleteBtn=document.createElement('button');
-	td3.appendChild(deleteBtn)
-	deleteBtn.addEventListerner('click',()=>{
-		list.remove('tr');
-		
-	})
-	td.textContent=title.value;
-	td1.textContent=author.value;
-	td2.textContent=isbn.value;
-	row.appendChild(td);
-	row.appendChild(td1);
-	row.appendChild(td2);
-	row.appendChild(td3);
-	list.appendChild(row);
-	
-	 
-})
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+let isbn = document.getElementById("isbn");
+let btn = document.getElementById("submit");
+let tbody = document.getElementById("book-list");
+btn.addEventListener("click", () => {
+  var tr = document.createElement("tr");
+  var td1 = document.createElement("td");
+  var td2 = document.createElement("td");
+  var td3 = document.createElement("td");
+  var td4 = document.createElement("td");
+  var removeBtn = document.createElement("button");
+  td4.append(removeBtn);
+  removeBtn.classList.add("delete");
+  removeBtn.textContent = "Delete";
+  td1.textContent = title.value;
+  td2.textContent = author.value;
+  td3.textContent = isbn.value;
+  removeBtn.addEventListener("click", () => {
+    tbody.removeChild(tr);
+  });
+
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+  tr.appendChild(td4);
+  tbody.appendChild(tr);
+
+  title.value = "";
+  author.value = "";
+  isbn.value = "";
+});
